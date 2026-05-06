2bb98d2f6fb439109af56b9f01cfe39630a5164c
Hany Naji <hany.tamman77@gmail.com>
2026-05-06 13:53:00 +0300

    Fix translated clinic name column migration

--- addons/pt_clinic/models/branch.py
+++ addons/pt_clinic/models/branch.py
@@ -11,6 +11,30 @@ class PtBranch(models.Model):
     )
 
     name = fields.Char(string="Branch", required=True, translate=True)
+
+    def init(self):
+        super().init()
+        self.env.cr.execute(
+            """
+            SELECT udt_name
+              FROM information_schema.columns
+             WHERE table_name = 'pt_branch'
+               AND column_name = 'name'
+            """
+        )
+        column_info = self.env.cr.fetchone()
+        if column_info and column_info[0] != "jsonb":
+            self.env.cr.execute(
+                """
+                ALTER TABLE pt_branch
+                ALTER COLUMN name TYPE jsonb
+                USING CASE
+                    WHEN name IS NULL THEN NULL
+                    ELSE jsonb_build_object('en_US', name::text)
+                END
+                """
+            )
+
     code = fields.Char(string="Branch Code", required=True)
     clinic_company_id = fields.Many2one(
         "res.company", string="Company", required=True, default=lambda self: self.env.company, index=True
@@ -20,5 +44,4 @@ class PtBranch(models.Model):
     email = fields.Char(string="Email")
     address = fields.Char(string="Address")
     active = fields.Boolean(string="Active", default=True)
-
 
--- addons/pt_clinic/models/dashboard.py
+++ addons/pt_clinic/models/dashboard.py
@@ -11,6 +11,30 @@ class PtClinicDashboard(models.Model):
     _description = "Wiqaya Clinic Dashboard"
 
     name = fields.Char(default="Wiqaya", required=True, translate=True)
+
+    def init(self):
+        super().init()
+        self.env.cr.execute(
+            """
+            SELECT udt_name
+              FROM information_schema.columns
+             WHERE table_name = 'pt_clinic_dashboard'
+               AND column_name = 'name'
+            """
+        )
+        column_info = self.env.cr.fetchone()
+        if column_info and column_info[0] != "jsonb":
+            self.env.cr.execute(
+                """
+                ALTER TABLE pt_clinic_dashboard
+                ALTER COLUMN name TYPE jsonb
+                USING CASE
+                    WHEN name IS NULL THEN NULL
+                    ELSE jsonb_build_object('en_US', name::text)
+                END
+                """
+            )
+
     patient_count = fields.Integer(compute="_compute_counts")
     appointment_today_count = fields.Integer(compute="_compute_counts")
     assessment_count = fields.Integer(compute="_compute_counts")
